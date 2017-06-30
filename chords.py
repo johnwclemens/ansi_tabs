4ddb58632bf1676d199290a924da5e6546e0926f
John Clemens <jwclemens@yahoo.com>
2017-06-30 11:19:30 -0500

    study chords part 2

--- chords.py
+++ chords.py
@@ -161,10 +161,11 @@ class Chords(object):
             if len(chordName) > 0:
                 if len(chordName) > 1 and ( chordName[1] == '#' or chordName[1] == 'b' ):
                     chordName = chordName[0] + chordName[2:]
-                print('printChord() colorize name={}, len={}'.format(chordName, len(chordName)), file=self.tabsObj.dbgFile)
-                i = 0
-                while i < len(chordName):
-                    print('printChord() colorizing name={}, len={}, i={}'.format(chordName, len(chordName), i), file=self.tabsObj.dbgFile)
+#                print('printChord() colorize name={}, len={}'.format(chordName, len(chordName)), file=self.tabsObj.dbgFile)
+                for i in range(len(chordName)):
+#                i = 0
+#                while i < len(chordName):
+#                    print('printChord() colorizing name={}, len={}, i={}'.format(chordName, len(chordName), i), file=self.tabsObj.dbgFile)
                     style = self.tabsObj.styles['NAT_CHORD']
                     if i == 0:
                         if len(imap['R']) > 1:
@@ -180,16 +181,16 @@ class Chords(object):
                                     style = self.tabsObj.styles['FLT_CHORD']
                     elif chordName[i] == 'm' or 'dim' in chordName and chordName[i] == 'd' or chordName[i] == 'i':
                         style = self.tabsObj.styles['FLT_CHORD']
-                    elif chordName[i] == '+':
-                        chordName = chordName[0:i] + chordName[i+1:]
-                        style = self.tabsObj.styles['SHP_CHORD']
-                        print('printChord() colorized name={}, len={}, i={}'.format(chordName, len(chordName), i), file=self.tabsObj.dbgFile)
+#                    elif chordName[i] == '+':
+#                        chordName = chordName[0:i] + chordName[i+1:]
+#                        style = self.tabsObj.styles['SHP_CHORD']
+#                        print('printChord() colorized name={}, len={}, i={}'.format(chordName, len(chordName), i), file=self.tabsObj.dbgFile)
                     self.tabsObj.prints(chordName[i], i + row, col, style)
-                    i += 1
+#                    i += 1
 #                    self.moveTo()
                 break
 
-    def getChordName(self, imap):
+    def getChordName_ORIG(self, imap):
         '''Calculate chord name.'''
         r = imap['R']
         if 'R' in imap:
@@ -201,7 +202,8 @@ class Chords(object):
                         if   'b7' in imap:                    return '{}7'.format(r)
                         elif  '7' in imap:                    return '{}M7'.format(r)
                         elif  '6' in imap or '13' in imap:    return '{}6'.format(r)
-                        elif  '2' in imap or  '9' in imap:    return '{}+9'.format(r)
+                        elif  '4' in imap or '11' in imap:    return '{}4'.format(r)
+                        elif  '2' in imap or  '9' in imap:    return '{}2'.format(r)
                     elif len(imap) == 5:
                         if 'b7' in imap:
                             if   '2' in imap or  '9' in imap: return '{}9'.format(r)
@@ -217,7 +219,8 @@ class Chords(object):
                         if   'b7' in imap:                    return '{}m7'.format(r)
                         elif  '7' in imap:                    return '{}mM7'.format(r)
                         elif  '6' in imap or '13' in imap:    return '{}m6'.format(r)
-                        elif  '2' in imap or  '9' in imap:     return '{}m+9'.format(r)
+                        elif  '4' in imap or '11' in imap:    return '{}m4'.format(r)
+                        elif  '2' in imap or  '9' in imap:    return '{}m2'.format(r)
                     elif len(imap) == 5:
                         if 'b7' in imap:
                             if   '2' in imap or  '9' in imap: return '{}m9'.format(r)
@@ -250,72 +253,147 @@ class Chords(object):
                     elif  '7' in imap:                        return '{}mM7n5'.format(r)
         return ''
 
+    def getChordName(self, imap):
+        '''Calculate chord name.'''
+        r = imap['R']
+        if '5' in imap: 
+            if len(imap) == 2:                            return '{}5'.format(r)    # Power5
+            elif 'M3' in imap:
+                if len(imap) == 3:                        return '{}'.format(r)     # Maj
+                elif len(imap) == 4:
+                    if   'b7' in imap:                    return '{}7'.format(r)    # Dom7
+                    elif  '7' in imap:                    return '{}M7'.format(r)   # Maj7
+                    elif  '2' in imap or  '9' in imap:    return '{}2'.format(r)    # Add2
+                    elif  '4' in imap or '11' in imap:    return '{}4'.format(r)    # Add4
+                    elif  '6' in imap or '13' in imap:    return '{}6'.format(r)    # Add6
+                elif len(imap) == 5:
+                    if 'b7' in imap:
+                        if   '2' in imap or  '9' in imap: return '{}9'.format(r)    # 9
+                        elif '4' in imap or '11' in imap: return '{}11n9'.format(r) # 11(no9)
+                        elif '6' in imap or '13' in imap: return '{}13n9'.format(r) # 13(no9)
+                    elif '7' in imap:
+                        if   '2' in imap or  '9' in imap: return '{}M9'.format(r)   # Maj9
+                        elif '4' in imap or '11' in imap: return '{}M11n9'.format(r)  # Maj11(no9)
+                        elif '6' in imap or '13' in imap: return '{}M13n9'.format(r)  # Maj13(no9)
+                elif len(imap) == 6:
+                    if 'b7' in imap and ('2' in imap or '9' in imap):
+                        if   '4' in imap or '11' in imap: return '{}11'.format(r)   # 11
+                        elif '6' in imap or '13' in imap: return '{}13'.format(r)   # 13
+                    elif '7' in imap:
+                        if   '4' in imap or '11' in imap: return '{}M11'.format(r)  # Maj11
+                        elif '6' in imap or '13' in imap: return '{}M13'.format(r)  # Maj13
+            elif 'm3' in imap:
+                if len(imap) == 3:                        return '{}m'.format(r)    # Min
+                elif len(imap) == 4:
+                    if   'b7' in imap:                    return '{}m7'.format(r)   # MinDom7
+                    elif  '7' in imap:                    return '{}mM7'.format(r)  # MinMaj7
+                    elif  '2' in imap or  '9' in imap:    return '{}m2'.format(r)   # MinAdd2
+                    elif  '4' in imap or '11' in imap:    return '{}m4'.format(r)   # MinAdd4
+                    elif  '6' in imap or '13' in imap:    return '{}m6'.format(r)   # MinAdd6
+                elif len(imap) == 5:
+                    if 'b7' in imap:
+                        if   '2' in imap or  '9' in imap: return '{}m9'.format(r)   # Min9
+                        elif '4' in imap or '11' in imap: return '{}m11'.format(r)  # Min11
+                        elif '6' in imap or '13' in imap: return '{}m13'.format(r)  # Min13
+            elif len(imap) == 3:
+                if    '2' in imap or  '9' in imap:        return '{}s2'.format(r)   # sus2
+                elif  '4' in imap or '11' in imap:        return '{}s4'.format(r)   # sus4
+            elif len(imap) == 4:
+                if   'b7' in imap:
+                    if    '2' in imap or  '9' in imap:    return '{}7s2'.format(r)  # Dom7sus2
+                    elif  '4' in imap or '11' in imap:    return '{}7s4'.format(r)  # Dom7sus4
+        elif 'b5' in imap:
+            if 'm3' in imap:
+                if len(imap) == 3:                        return '{}o'.format(r)    # Dim
+                elif len(imap) == 4:
+                    if   'b7' in imap:                    return '{}07'.format(r)   # HalfDim7
+                    elif  '6' in imap:                    return '{}o7'.format(r)   # Dim7
+            elif 'M3' in imap:
+                if len(imap) == 4:
+                    if 'b7' in imap:                      return '{}7b5'.format(r)  # Dom7Dim5
+        elif 'a5' in imap:
+            if 'M3' in imap:
+                if len(imap) == 3:                        return '{}+'.format(r)    # Aug
+                elif len(imap) == 4:
+                    if   'b7' in imap:                    return '{}+7'.format(r)   # Aug7
+                    elif  '7' in imap:                    return '{}+M7'.format(r)  # AugMaj7
+        # Maybe omit all the n5 (no 5th) chords for simplicity
+        elif 'M3' in imap:
+            if len(imap) == 3:
+                if   'b7' in imap:                        return '{}7n5'.format(r)
+                elif  '7' in imap:                        return '{}M7n5'.format(r)
+            elif len(imap) == 4:
+                if   'b7' in imap:
+                    if '9' in imap:                       return '{}9n5'.format(r)
+        elif 'm3' in imap:
+            if len(imap) == 3:
+                if   'b7' in imap:                        return '{}m7n5'.format(r)
+                elif  '7' in imap:                        return '{}mM7n5'.format(r)
+        return ''
+        
 '''
 Chord Naming Conventions:
-| ----------------------------------------------------|
-| Long      | Short | Intervals      | Notes          |
-| ----------------------------------------------------|
-| CMaj      | C     | R 3 5          | C E G          |
-| CMin      | Cm    | R m3 5         | C Eb G         |
-| CAug      | C+    | R 3 a5         | C E G#         |
-| CDim      | Co    | R m3 b5        | C Eb G#        |
-| ----------------------------------------------------|
-| CMaj7     | CM7   | R 3 5 7        | C E G B        |
-| CDom7     | C7    | R 3 5 b7       | C E G Bb       |
-| CMin7     | Cm7   | R m3 5 b7      | C Eb G Bb      |
-| CMinMaj7  | CmM7  | R m3 5 7       | C Eb G B       |
-| CAugMaj7  | C+M7  | R 3 a5 7       | C E G# B       |
-| CAug7     | C+7   | R 3 a5 b7      | C E G# Bb      |
-| CHDim7    | C07   | R m3 b5 b7     | C Eb Gb Bb     |
-| CDim7     | Co7   | R m3 b5 bb7    | C Eb Gb Bbb    |
-| C7Dim5    | C7b5  | R 3 b5 b7      | C E Gb Bb      |
-| ----------------------------------------------------|
-| CMaj9     | CM9   | R 3 5 7 9      | C E G B D      |
-| CDom9     | C9    | R 3 5 b7 9     | C E G Bb D     |
-| CMinMaj9  | CmM9  | R m3 5 7 9     | C Eb G B D     |
-| CMinDom9  | Cm9   | R m3 5 b7 9    | C Eb G Bb D    |
-| CAugMaj9  | C+M9  | R 3 a5 7 9     | C E G# B D     |
-| CAugDom9  | C+9   | R 3 a5 b7 9    | C E G# Bb D    |
-| CHDim9    | C09   | R m3 b5 b7 9   | C Eb Gb Bb D   |
-| CHDimMin9 | C0b9  | R m3 b5 b7 9b  | C Eb Gb Bb Db  |
-| CDim9     | Co9   | R m3 b5 bb7 9  | C Eb Gb Bbb D  |
-| CDimMin9  | Cob9  | R m3 b5 bb7 b9 | C Eb Gb Bbb Db |
-| ----------------------------------------------------|
-| CMaj11    | CM11  | R 3 5 7 9 11   | C E G B D F    |
-| CDom11    | C11   | R 3 5 b7 9 11  | C E G Bb D F   |
-| CMinMaj11 | CmM11 | R m3 5 7 9 11  | C Eb G Bb D F  |
-| ----------------------------------------------------|
+| ----------------------------------------------------------|
+| Long      | Short | Intervals          | Notes            | NoR | No3 | No5 |
+| ----------------------------------------------------------|
+| CMaj      | C     | R 3 5              | C E G            |
+| CMin      | Cm    | R m3 5             | C Eb G           |
+| CAug      | C+    | R 3 a5             | C E G#           |
+| CDim      | Co    | R m3 b5            | C Eb Gb          |
+| ----------------------------------------------------------|
+| CMaj7     | CM7   | R 3 5 7            | C E G B          |
+| CDom7     | C7    | R 3 5 b7           | C E G Bb         |
+| CMin7     | Cm7   | R m3 5 b7          | C Eb G Bb        |
+| CMinMaj7  | CmM7  | R m3 5 7           | C Eb G B         |
+| CAugMaj7  | C+M7  | R 3 a5 7           | C E G# B         |
+| CAug7     | C+7   | R 3 a5 b7          | C E G# Bb        |
+| CHDim7    | C07   | R m3 b5 b7         | C Eb Gb Bb       |
+| CDim7     | Co7   | R m3 b5 bb7        | C Eb Gb Bbb      |
+| C7Dim5    | C7b5  | R 3 b5 b7          | C E Gb Bb        |
+| ----------------------------------------------------------|
+| CMaj9     | CM9   | R 3 5 7 9          | C E G B D        |
+| CDom9     | C9    | R 3 5 b7 9         | C E G Bb D       |
+| CMinMaj9  | CmM9  | R m3 5 7 9         | C Eb G B D       |
+| CMinDom9  | Cm9   | R m3 5 b7 9        | C Eb G Bb D      |
+| CAugMaj9  | C+M9  | R 3 a5 7 9         | C E G# B D       |
+| CAugDom9  | C+9   | R 3 a5 b7 9        | C E G# Bb D      |
+| CHDim9    | C09   | R m3 b5 b7 9       | C Eb Gb Bb D     |
+| CHDimMin9 | C0b9  | R m3 b5 b7 9b      | C Eb Gb Bb Db    |
+| CDim9     | Co9   | R m3 b5 bb7 9      | C Eb Gb Bbb D    |
+| CDimMin9  | Cob9  | R m3 b5 bb7 b9     | C Eb Gb Bbb Db   |
+| ----------------------------------------------------------|
+| CMaj11    | CM11  | R 3 5 7 9 11       | C E G B D F      |
+| CDom11    | C11   | R 3 5 b7 9 11      | C E G Bb D F     |
+| CMinMaj11 | CmM11 | R m3 5 7 9 11      | C Eb G B D F     |
+| CMin11    | Cm11  | R m3 5 b7 9 11     | C Eb G Bb D F    |
+| CAugMaj11 | C+M11 | R 3 a5 7 9 11      | C E G# B D F     |
+| CAug11    | C+11  | R 3 a5 b7 9 11     | C E G# Bb D F    |
+| CHDim11   | C011  | R m3 b5 b7 b9 11   | C Eb Gb Bb Db F  |
+| CDim11    | Co11  | R m3 b5 bb7 b9 b11 | C Eb Gb Bbb Db F |
+| ----------------------------------------------------------|
+| CMaj13    | CM13  | R 3 5 7 9 11 13    | C E G B D F A    |
+| CDom13    | C13   | R 3 5 b7 9 11 13   | C E G Bb D F A   |
+| CMinMaj13 | CmM13 | R m3 5 7 9 11 13   | C Eb G B D F A   |
+| CMinDom13 | Cm13  | R m3 5 b7 9 11 13  | C Eb G Bb D F A  |
+| CAugMaj13 | C+M13 | R 3 a5 7 9 11 13   | C E G# B D F A   |
+| CAugDom13 | C+13  | R 3 a5 b7 9 11 13  | C E G# Bb D F A  |
+| CHDim13   | C013  | R m3 b5 b7 9 11 13 | C Eb Gb Bb D F A |
+| ----------------------------------------------------------|
+| CPower5   | C5    | R 5                | C G              |
+| C7no3     | C7no3 | R 5 7b             | C G Bb           |
+| ----------------------------------------------------------|
+| Csus2     | Cs2   | R 2 5              | C D G            |
+| C7sus2    | C7s2  | R 2 5 b7           | C D G Bb         |
+| C9sus2    | C9s2  | R 2 5 b7 9         | C D G Bb D       |
+| Csus4     | Cs4   | R 4 5              | C F G            |
+| C7sus4    | C7s4  | R 4 5 b7           | C F G Bb         |
+| C9sus4    | C9s4  | R 4 5 b7 9         | C F G Bb D       |
+| ----------------------------------------------------------|
+| Cadd9     | C2    | R 2 3 5            | C D E G          |
+| Cadd11    | C4    | R 3 4 5            | C E F G          |
+| Cadd13    | C6    | R 3 5 6            | C E G A          |
+| CAdd6Add9 | C6/9  | R 3 5 6 9          | C E G A D        |
 
-
-Long      | Short | Intervals      | Notes
----------------------------------------------------
-CMaj      | C     | R 3 5          | C E G
-CMin      | Cm    | R m3 5         | C Eb G
-CAug      | C+    | R 3 a5         | C E G#
-CDim      | Co    | R m3 b5        | C Eb G#
-
-CDom7     | C7    | R 3 5 b7       | C E G Bb
-CMaj7     | CM7   | R 3 5 7        | C E G B
-CMin7     | Cm7   | R m3 5 b7      | C Eb G Bb
-CMinMaj7  | CmM7  | R m3 5 7       | C Eb G B
-CAugMaj7  | C+M7  | R 3 a5 7       | C E G# B
-CAug7     | C+7   | R 3 a5 b7      | C E G# Bb
-CHDim7    | C07   | R m3 b5 b7     | C Eb Gb Bb
-CDim7     | Co7   | R m3 b5 bb7    | C Eb Gb Bbb
-C7Dim5    | C7b5  | R 3 b5 b7      | C E Gb Bb
-
-CMaj9     | CM9   | R 3 5 7 9      | C E G B D
-CDom9     | C9    | R 3 5 b7 9     | C E G Bb D
-CMinMaj9  | CmM9  | R m3 5 7 9     | C Eb G B D
-CMinDom9  | Cm9   | R m3 5 b7 9    | C Eb G Bb D
-CAugMaj9  | C+M9  | R 3 a5 7 9     | C E G# B D
-CAugDom9  | C+9   | R 3 a5 b7 9    | C E G# Bb D
-CHDim9    | C09   | R m3 b5 b7 9   | C Eb Gb Bb D
-CHDimMin9 | C0b9  | R m3 b5 b7 9b  | C Eb Gb Bb Db
-CDim9     | Co9   | R m3 b5 bb7 9  | C Eb Gb Bbb D
-CDimMin9  | Cob9  | R m3 b5 bb7 b9 | C Eb Gb Bbb Db
-
----------------------------------------------------
 
 C5   : R 5            : C G            : 5
 C    : R M3 5         : C E G          : Maj
@@ -347,12 +425,4 @@ C7+5 : C E G# Bb      : 7Aug5
 C7-5 : C E Gb Bb      : 7Dim5
 C7+9 : C E G Bb D#    : 7#9
 C7-9 : C E G Bb Db    : 7b9
-
-Cs2 : C D G           : sus2
-Cs4 : C F G           : sus4
-C2  : C D E G         : 2
-C4  : C E F G         : 4
-
-C6/9 : C E G A D
-
 '''
