bd4d54a2178ffa3f03e1158cf51d4e433761294c
johnwclemens <Jwclemens@yahoo.com>
2018-04-22 23:31:41 -0500

    add double bass

--- strings.py
+++ strings.py
@@ -5,24 +5,25 @@ import notes, collections
 class Strings(object):
     '''Model the musical instrument strings and how they are tuned in ascending order.'''
     
-    def _initAliases(self):
+    def _initAliases(self):  # how to model mandolin and 12 string guitar?
         '''Register aliases for specific string spellings.  Edit this method to add, remove, or modify aliases as desired.'''
         self.ALIASES = {}
-        self.ALIASES['GUITAR']                    = ['E2A2D3G3B3E4']
+        self.ALIASES['GUITAR']                    = ['E2A2D3G3B3E4']   # standard 6 string
         self.ALIASES['GUITAR_DADGAD']             = ['D2A2D3G3A3D4']
         self.ALIASES['GUITAR_OPEN_D_VESTAPOL']    = ['D2A2D3F#3A3D4']
         self.ALIASES['GUITAR_OPEN_G_OVERTONE']    = ['G2G2D3G3B3D4']
         self.ALIASES['GUITAR_OPEN_GMIN_OVERTONE'] = ['G2G2D3G3Bb3D4']
         self.ALIASES['TENOR_GUITAR']              = ['C3G3D4A4']
-        self.ALIASES['UKULELE']                   = ['G4C4E4A4']
-        self.ALIASES['BASS']                      = ['E1A1D2G2']
+        self.ALIASES['UKULELE']                   = ['G4C4E4A4']       # 1 of many tunings, all notes in the same octave with the G note out of order
+        self.ALIASES['BASS']                      = ['E1A1D2G2']       # electric bass not acoustic double bass
         self.ALIASES['BASS_5_STRING']             = ['B0E1A1D2G2']
         self.ALIASES['BASS_5_STRING_TENOR']       = ['E1A1D2G2C3']
         self.ALIASES['BASS_6_STRING']             = ['B0E1A1D2G2C3']
+        self.ALIASES['DOUBLE_BASS']               = ['E1A1D2G2']       # acoustic double bass not electric bass
         self.ALIASES['CELLO']                     = ['C2G2D3A3']
         self.ALIASES['VIOLA']                     = ['C3G3D4A4']
         self.ALIASES['VIOLIN']                    = ['G3D4A4E5']
-    
+
     def __init__(self, dbgFile, spelling=None, alias=None):
         '''The alias argument overrides the spelling argument.  e.g. alias=['GUITAR'] -> strings=['E2A2D3G3B3E4'] represents a standard 6 string guitar tuning.'''
         self.dbgFile = dbgFile
@@ -50,21 +51,21 @@ class Strings(object):
     @property
     def map(self):
         return self._map
-    
+
     @property
     def keys(self):
         return self._keys
-        
+
     @map.setter
     def map(self):
         '''Return map of string note name -> note index e.g. a standard 6 string guitar tuning { E2:28 A2:33 D3:38 G3:43 B3:47 E4:52 }.'''
         return self._map
-        
+
     @keys.setter
     def keys(self):
         '''Return the keys sorted in ascending order.'''
         return self._keys
-        
+
     def _parseSpelling(self, spelling):
         '''Parse string spelling into map of note name -> note index.'''
         key = None
@@ -102,7 +103,7 @@ class Strings(object):
         for k in self.keys:
             print(' {}:{}'.format(k, self.map[k]), end='', file=self.dbgFile)
         print(' }', file=self.dbgFile)
-        
+
     def _mapKeyFunc(self, inKey):
         '''Internal method for sorting the map keys in ascending order.'''
         return self.map[inKey]
